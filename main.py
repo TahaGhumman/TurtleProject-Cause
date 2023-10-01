bc4242afa35ce58249c24623c0865e5014d63490
Taha Ghumman <mtghumma@ualberta.ca>
2023-10-01 17:56:41 -0600

    Extra cleaning

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import turtle as t
 
-def createRectangle(sizeX, sizeY, position, fillColor):
+def createRectangle(sizeX, sizeY, position, fillColor): # Creates rectangular shapes necessary for the creation of the flag
     t.penup()
     t.setpos(position)
     t.fillcolor(fillColor)
     t.pencolor(fillColor)
     t.begin_fill()
     t.pendown()
-    t.degrees(360)
-    t.forward(sizeY)
-    t.right(90)
-    t.forward(sizeX)
-    t.right(90)
-    t.forward(sizeY)
-    t.right(90)
-    t.forward(sizeX)
+
+    for i in range(1, 3): # Repeats repeatable processes
+        t.forward(sizeY)
+        t.right(90)
+        t.forward(sizeX)
+        t.right(90)
+
+    t.left(90)
+
     t.end_fill()
     return
 
@@ -23,9 +24,11 @@ flagSizeY = 550
 
 createRectangle(flagSizeX, flagSizeY, (-300, 200), "green")
 t.back(flagSizeX/3)
+
 createRectangle(flagSizeY, flagSizeX/3, t.pos(), "black")
 t.right(90)
 t.back(flagSizeX/3)
+
 createRectangle(flagSizeY, flagSizeX/3, t.pos(), "white")
-#t.done()
+
 t.exitonclick()
