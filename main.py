560420654d1e40b287bcf46cdbd58b93856f8acf
Taha Ghumman <mtghumma@ualberta.ca>
2023-10-02 20:03:14 -0600

    Cleaning Code

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
 import turtle as t
+from time import sleep
+from typing import *
 
+"""
+Creating the Palestinian Flag for the crisis in Palestine/Israel using Python Turtle. 
 
-# Flag - Taha Ghumman
-
-# Rectangle Function
-def createRectangle(sizeX, sizeY, position, fillColor, penColor = None):
-    t.penup()
-    t.setpos(position)
-    t.fillcolor(fillColor)
-    t.pencolor(penColor or fillColor)
-    t.begin_fill()
-    t.pendown()
-
-    for i in range(1, 3): # Repeats repeatable processes
-        t.forward(sizeY)
-        t.right(90)
-        t.forward(sizeX)
-        t.right(90)
-
-    t.left(90)
-
-    t.end_fill()
-    return
+Focuses on exerting skills within if statements, for loops, variables, and understanding Python Turtle. 
+"""
 
 #Flag Size
 flagSizeX = 350
@@ -32,6 +17,52 @@ redColor = (238,42,53)
 blackColor = (0, 0, 0)
 greenColor = (0,151,54)
 whiteColor = (255,255,255)
+
+# Flag - Taha Ghumman
+
+# Creates Flame Wisp - Rashed Amani
+def createWisp(radius):
+    """
+    Creates a triangular 'wisp' shape.
+
+    :params radius: the length from the centre of the circle, creates the width of the wisp.
+    
+    """
+    t.circle(radius, 90)
+    t.left(180)
+    t.circle(radius, 90)
+
+# Rectangle Function - Taha Ghumman
+def createRectangle(sizeX: int, sizeY: int, position: tuple, fillColor: Any, penColor:Any = None):
+    """
+    Creates a rectangle object based on your given 'x' and 'y' sizes and positions it as 'position'. 
+    It uses 'fillColor' as the color to use within of the rectangle and 'penColor' for the outline.
+    'penColor' is not a needed argument as it defaults to "None" and thus uses the fillColor.
+
+    :params sizeX: The x size of the rectangle
+            sizeY: The y size of the rectangle
+            position: The (x, y) coordinate within the Python Window
+            fillColor: The color used to fill the rectangle
+            penColor: The penColor used to outline the rectangle
+    """
+
+    t.penup()
+    t.setpos(position)
+    t.fillcolor(fillColor)
+    t.pencolor(penColor or fillColor)
+    t.begin_fill()
+    t.pendown()
+
+    for i in range(1, 3): # Repeats creating the borders of the rectangle
+        t.forward(sizeY)
+        t.right(90)
+        t.forward(sizeX)
+        t.right(90)
+
+    t.left(90)
+
+    t.end_fill()
+    return
 
 t.speed(0)
 t.colormode(255)
@@ -57,7 +88,6 @@ t.forward(flagSizeY/2.22)
 t.left(-90)
 t.forward(flagSizeY/2.22)
 t.end_fill()
-
 
 
 # Fist - Troy Williams
@@ -108,5 +138,38 @@ t.pendown()
 t.write("PALESTINE", font=("Futura", 40, "bold"))
 t.penup()
 
+# Flames - Rashed Omani
+window = t.Screen()
+window.title('Free Palestine')
 
+t.color('red', 'orange')
+
+t.pensize(1)
+t.width()
+t.pencolor('red')
+t.fillcolor('orange')
+
+t.penup()
+t.setposition(-300, 200)
+t.speed(0)
+t.begin_fill()
+
+t.pendown()
+
+# Create Blinking Effect
+for i in range(20):
+    t.begin_fill()
+    t.setheading(0)
+    if i % 2 == 0:
+        t.color("orange", "red")
+    else:
+        t.color("red", "orange")
+    for x in range(5):
+        createWisp(55)
+    t.left(180)
+    t.forward(5 * 55 * 2)
+    t.end_fill()
+    sleep(1)
+
+# Finish
 t.exitonclick()
